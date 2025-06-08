11e4094c00983ecbd4902afd37a6d99fb5dcf231
Shunfa Xu <gouwa5700@gmail.com>
2025-06-08 16:39:46 -0700

    use a different website that doesn't give 403 Forbiden errors

--- blogs/temptation_of_wolves/temptation_of_wolves_page.rb
+++ blogs/temptation_of_wolves/temptation_of_wolves_page.rb
@@ -5,10 +5,10 @@ require "pry-byebug"
 class TemptationOfWolvesPage < Page
   def initialize(page_url, page_html)
     super(page_url, page_html)
-    match_data = page_url.match(/book\/1933\/[0-9]{5}/)
-    current_page_number = match_data.nil? ? 54087 : match_data[0].split("/").last.to_i
-    @next_page_url = "https://99csw.com/book/1933/#{current_page_number + 1}.htm"
-    @previous_page_url = "https://99csw.com/book/1933/#{current_page_number - 1}.htm"
+    match_data = page_url.match(/book4\/9246\/[0-9]{6}/)
+    current_page_number = match_data.nil? ? 204538 : match_data[0].split("/").last.to_i
+    @next_page_url = "https://www.kanunu8.com/book4/9246/#{current_page_number + 1}.html"
+    @previous_page_url = "https://www.kanunu8.com/book4/9246/#{current_page_number - 1}.html"
     @post_urls = [@page_url]
   end
 end
--- blogs/temptation_of_wolves/temptation_of_wolves_post.rb
+++ blogs/temptation_of_wolves/temptation_of_wolves_post.rb
@@ -4,9 +4,9 @@ require_relative '../../src/post'
 class TemptationOfWolvesPost < Post
   def initialize(post_url, post_html)
     super(post_url, post_html)
-    @title = post_html.css("#c_title").text
+    @title = post_html.css("body > div > table:nth-of-type(4) > tbody > tr:nth-of-type(1) > td").text
     @published_date = '2004-08-01'
-    @content = post_html.css("#content").children
+    @content = post_html.css("body > div > table:nth-of-type(5)").children
     @author = "（韩）可爱淘"
   end
 end
