5890e874705ad35acefcc0daeb93e55d39113319
Shunfa Xu <gouwa5700@gmail.com>
2025-06-08 16:57:42 -0700

    update css selectors for title & post

--- blogs/temptation_of_wolves/temptation_of_wolves_post.rb
+++ blogs/temptation_of_wolves/temptation_of_wolves_post.rb
@@ -4,9 +4,9 @@ require_relative '../../src/post'
 class TemptationOfWolvesPost < Post
   def initialize(post_url, post_html)
     super(post_url, post_html)
-    @title = post_html.css("body > div > table:nth-of-type(4) > tbody > tr:nth-of-type(1) > td").text
+    @title = post_html.css("body > div > table:nth-of-type(4) > tr:nth-of-type(1) > td > strong > font").text
     @published_date = '2004-08-01'
-    @content = post_html.css("body > div > table:nth-of-type(5)").children
+    @content = post_html.css("body > div > table:nth-of-type(5) > tr > td:nth-of-type(2) > p").children
     @author = "（韩）可爱淘"
   end
 end
