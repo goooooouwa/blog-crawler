019d670de457d2ffb7c5fc7dd1c7709f82e205cb
Shunfa Xu <gouwa5700@gmail.com>
2026-02-18 21:34:37 -0800

    switch to substack as medium forbid crawling

--- blogs/the_looking_glass/the_looking_glass_post.rb
+++ blogs/the_looking_glass/the_looking_glass_post.rb
@@ -4,9 +4,15 @@ require_relative '../../src/post'
 class TheLookingGlassPost < Post
   def initialize(post_url, post_html)
     super(post_url, post_html)
-    @title = post_html.css("[testid='storyTitle']").text
-    @published_date = post_html.at("meta[property='article:published_time']")['content']
-    @content = post_html.css("article").children
+    @title = post_html.css(".post-title").text
+    script_tags = post_html.css('script')
+    script_tags.each do |script|
+      text = script.content
+      if match = text.match(/"post_date\\":\\"([^\\]*)\\"/)
+        @published_date = match[1]
+      end
+    end
+    @content = post_html.css(".available-content").children
     @author = "Julie Zhuo"
   end
 end
